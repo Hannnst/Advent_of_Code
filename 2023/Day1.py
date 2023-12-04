7abd6a11f593c8bd0193df47e570e6bf73c6c5d0
Hanna N. Støen <hannanikolines@outlook.com>
2023-12-04 16:26:00 +0100

    day 2

--- 2023/Day1.py
+++ 2023/Day1.py
@@ -1,6 +1,96 @@
-""" The task text """
+from utils import readFile
+import re
 
+def find_spelled_out_numbers(line):
+    # Use regular expression to find spelled-out numbers
+    matches = re.findall(r'[a-zA-Z]*(zero|one|two|three|four|five|six|seven|eight|nine)[a-zA-Z]*', line)
 
-def christmasWonder():
+    # Convert spelled-out numbers to digits
+    numbers = [str(['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'].index(match)) for match in matches]
+
+    return numbers
+def artisticArtElfDecoder():
     """ What it do """
-    pass
+    calibrationValues = readFile("Files/day1_test.txt")
+    sum = 0
+    sum9000 = 0
+    for calibration in calibrationValues:
+        numbers = []
+        numbers9000 = []
+        potentialNumber = ""
+        for sign in calibration:
+            try:
+                if sign.isnumeric():
+                    numbers.append(int(sign))
+                else:
+                    print(potentialNumber)
+                    potentialNumber += sign
+                    if artisticArtElfDecoder9000(potentialNumber):
+                        print(potentialNumber)
+                        artisticArtElfDecoder9000(potentialNumber)
+                        numbers9000.append(artisticArtElfDecoder9000(potentialNumber))
+                        potentialNumber = ""
+            except:
+                pass
+        if (len(numbers) > 0):
+            sum += int(str(numbers[0]) + str(numbers.pop()))
+        if (len(numbers9000) > 1):
+            sum9000 += int(str(numbers9000[0]) + str(numbers9000.pop()))
+
+    return sum, sum9000
+
+def artisticArtElfDecoder9000(number):
+    numbers = {
+        "one" : 1,
+        "two" : 2,
+        "three" : 3,
+        "four" : 4,
+        "five" : 5,
+        "six" : 6,
+        "seven" : 7,
+        "eight" : 8,
+        "nine" : 9,
+        "ten" : 10
+    }
+    return numbers[number]
+
+import re
+
+def find_spelled_out_numbers(line):
+    matches = re.findall(r'[a-zA-Z]*(zero|one|two|three|four|five|six|seven|eight|nine)[a-zA-Z]*', line)
+    numbers = [str(['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'].index(match)) for match in matches]
+    return numbers
+
+def combine_numbers(numbers):
+    return int(''.join(numbers))
+
+def main():
+    lines = [
+        "two1nine",
+        "eightwothree",
+        "abcone2threexyz",
+        "xtwone3four",
+        "4nineeightseven2",
+        "zoneight234",
+        "7pqrstsixteen"
+    ]
+
+    combined_numbers = []
+
+    for line in lines:
+        numbers = find_spelled_out_numbers(line)
+
+        if numbers:
+            combined_number = combine_numbers(numbers)
+            combined_numbers.append(combined_number)
+
+    total_sum = sum(combined_numbers)
+
+    print("Combined Numbers:", combined_numbers)
+    print("Total Sum:", total_sum)
+
+if __name__ == "__main__":
+    main()
+
+
+print(artisticArtElfDecoder())
--- 2023/Day2.py
+++ 2023/Day2.py
@@ -1,6 +1,23 @@
-""" The task text """
+""" only 12 red cubes, 13 green cubes, and 14 blue cubes? """
+from collections import defaultdict
+from utils import readFile
 
-
-def christmasWonder():
+inventory = {
+    "red": 12,
+    "green": 13,
+    "blue": 14
+}
+def cubeGame(inventory):
     """ What it does """
-    pass
+    totalCubes = sum(inventory.values())
+
+    sentence = "Game 75: 13 blue, 20 red, 10 green; 3 green, 5 blue, 14 red; 9 red, 13 green, 7 blue; 1 blue, 15 red, 2 green; 11 blue, 2 green, 17 red; 11 red, 13 blue, 13 green"
+    for line in readFile("Files/day2_test.txt"):
+        # Extract color and count information from the sentence
+        for info in sentence.split(';'):
+            for cubeGrab in info.split(','):
+                color, count = cubeGrab.strip().split()
+                inventory[color] += int(count)
+
+cubeGame(inventory)
+print(totalCubes)
